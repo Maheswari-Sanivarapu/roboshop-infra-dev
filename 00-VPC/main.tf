216848daa8288c1908fda0d8cd659e97e34e92cf
maheswari <mahi.sgs22@gmail.com>
2025-06-24 20:38:10 +0530

    terraform

---
+++ 00-VPC/main.tf
@@ -0,0 +1,8 @@
+module "vpc" {
+    source = "git::https://github.com/mahi2298/terraform-aws-vpc-module.git?ref=main"
+    project = var.project
+    environment = var.environment
+    public_subnet_cidr = var.public_subnet_cidr
+    private_subnet_cidr = var.private_subnet_cidr
+    is_vpc_peering = true
+}
