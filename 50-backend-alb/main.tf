c5d0e1bf5545eb603df52875884b22a85fb7a7d7
maheswari <mahi.sgs22@gmail.com>
2025-06-25 23:11:20 +0530

    infra-dev

--- 50-backend-alb/main.tf
+++ 50-backend-alb/main.tf
@@ -1,8 +1,9 @@
 #roboshop-dev-backend-alb
 module "alb" {
     source = "terraform-aws-modules/alb/aws"
-    name = "${var.project}-${var.environment}-backend-alb"
+    version = "9.17.0"
     internal = true # means this load balancer will be create in private subnet only. internal = false means it will create in public subnet
+    name = "${var.project}-${var.environment}-backend-alb"
     vpc_id = local.vpc_id
     subnets = local.private_subnet_ids # creating the alb in private subnet
     security_groups = local.backend_alb_sg_id  # getting the backend sg id from 10-sg bcoz in 10-sg it is created
