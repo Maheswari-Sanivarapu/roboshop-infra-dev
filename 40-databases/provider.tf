89192896e518c2602c935707bee4a7e937be4baa
maheswari <mahi.sgs22@gmail.com>
2025-06-29 22:10:01 +0530

    roboshop infra dev

---
+++ 40-databases/provider.tf
@@ -0,0 +1,22 @@
+terraform {
+  required_providers {
+    aws = {
+      source = "hashicorp/aws"
+      version = "5.98.0"
+    }
+  }
+ backend "s3"{
+    bucket = "mahi-s3-bucket-dev"
+    key = "remote-state-dev-db"
+    region = "us-east-1"
+    encrypt = true 
+    use_lockfile = true
+  }
+}
+
+provider "aws" {
+  # Configuration options
+  # region
+  region = "us-east-1"
+  #region = "ap-south-1"
+}
