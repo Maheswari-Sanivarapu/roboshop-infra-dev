913ddc02c0c45f27db0db95b0956fb9d6ae00180
maheswari <mahi.sgs22@gmail.com>
2025-06-30 15:24:43 +0530

    infra dev

--- 30-vpn/main.tf
+++ 30-vpn/main.tf
@@ -7,6 +7,8 @@ resource "aws_instance" "vpn" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [local.openvpn_sg_id]
     subnet_id = local.public_subnet_id  # public_subnet_id taking from 00-VPC
+    #key_name = "daws-84s" # make sure this key exist in AWS
+    key_name = aws_key_pair.openvpn.key_name
     user_data = file("openvpn.sh") # creating the openvpn.sh and adding the required files to run openvpn and here user_data will run while creating the instance
     tags = merge(
         local.common_tags,
--- 40-databases/local.tf
+++ 40-databases/local.tf
@@ -1,6 +1,6 @@
 locals {
     ami_id = data.aws_ami.databases_ami.id
-    database_subnet_id = data.aws_ssm_parameter.database_subnet_id.value
+    database_subnet_id = split(",",data.aws_ssm_parameter.database_subnet_id.value)[0]
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
     mysql_sg_id = data.aws_ssm_parameter.mysql_sg_id.value
