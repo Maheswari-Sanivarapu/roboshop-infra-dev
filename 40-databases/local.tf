4f185d7120dc9baf49de26e98020759d98291fad
maheswari <mahi.sgs22@gmail.com>
2025-06-30 14:49:33 +0530

    infra dev

--- 40-databases/local.tf
+++ 40-databases/local.tf
@@ -1,5 +1,5 @@
 locals {
-    ami_id = data.aws_ami.databases_ami
+    ami_id = data.aws_ami.databases_ami.id
     database_subnet_id = data.aws_ssm_parameter.database_subnet_id.value
     mongodb_sg_id = data.aws_ssm_parameter.mongodb_sg_id.value
     redis_sg_id = data.aws_ssm_parameter.redis_sg_id.value
