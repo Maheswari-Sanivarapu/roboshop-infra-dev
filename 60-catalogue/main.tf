ebcdae7fa1b09766b6c0bfbb267059900ae389ea
maheswari <mahi.sgs22@gmail.com>
2025-07-01 22:37:28 +0530

    infra dev

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -18,7 +18,7 @@ resource "aws_instance" "catalogue" {
     ami = local.ami_id
     instance_type = "t2.micro"
     vpc_security_group_ids = [local.catalogue_sg_id]
-    subnet_id = [local.private_subnet_id]
+    subnet_id = local.private_subnet_id
     tags = merge(
         local.common_tags,
         {
