ed51fc03d03a5e851fa2113fb9af286649644b04
maheswari <mahi.sgs22@gmail.com>
2025-07-02 21:26:46 +0530

    catalogue

--- 60-catalogue/main.tf
+++ 60-catalogue/main.tf
@@ -156,9 +156,9 @@ resource "aws_autoscaling_group" "catalogue" {
                 Name = "${var.project}-${var.environment}-catalogue"
             }
         )
-        content {
-            key = each.key
-            value = each.value
+        content{
+            key = tag.key
+            value = tag.value
             propagate_at_launch = true
         }
     }
