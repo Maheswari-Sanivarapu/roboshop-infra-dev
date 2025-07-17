11a7348e03a600919dbb6647cb19175c7ef4b25c
maheswari <mahi.sgs22@gmail.com>
2025-07-17 10:39:35 +0530

    roboshop

--- 20-bastion/main.tf
+++ 20-bastion/main.tf
@@ -3,6 +3,7 @@ resource "aws_instance" "bastion" {
     instance_type = "t2.micro"
     vpc_security_group_ids = [local.bastion_sg_id]
     subnet_id = local.public_subnet_id # creating the bastion in public subnet
+    user_data = file("bastion.sh")
     tags = merge (
         local.common_tags,
         {
--- 91-cdn/main.tf
+++ 91-cdn/main.tf
@@ -1,12 +1,13 @@
 resource "aws_cloudfront_distribution" "roboshop" {
     origin {
-        domain_name = "cdn.${var.route53_domain_name}"
+        domain_name = "cdn.${var.route53_domain_name}" # cdn.pavithra.fun here it is cdn domain name
         custom_origin_config {
+            http_port              = 80
             https_port             = 443
             origin_protocol_policy = "https-only"
             origin_ssl_protocols   = ["TLSv1.2"]
     }
-    origin_id = "dev.${var.route53_domain_name}"
+    origin_id = "dev.${var.route53_domain_name}" # frontend_alb domain name
     }
     enabled = true
     aliases = ["cdn.${var.route53_domain_name}"]
@@ -16,20 +17,21 @@ resource "aws_cloudfront_distribution" "roboshop" {
         cached_methods   = ["GET", "HEAD"]
         target_origin_id = "*.${var.route53_domain_name}"
         viewer_protocol_policy = "https-only"
-        cache_policy_id  = data.aws_cloudfront_cache_policy.cacheDisable.id
+        cache_policy_id  = data.aws_cloudfront_cache_policy.cacheDisable.id # here we are not caching the url so taking the data from aws like disablingcache
     }
     ordered_cache_behavior {
-        path_pattern           = "/media/*"
+        path_pattern     = "/media/*"
         allowed_methods  = ["GET", "HEAD","OPTIONS"]
         cached_methods   = ["GET", "HEAD","OPTIONS"]
         target_origin_id = "dev.${var.route53_domain_name}"
         viewer_protocol_policy = "https-only"
-        cache_policy_id  = data.aws_cloudfront_cache_policy.cacheEnable.id
+        cache_policy_id  = data.aws_cloudfront_cache_policy.cacheEnable.id # here we are caching the url so taking the data from aws like enablingcache
     }
+
     restrictions {
         geo_restriction {
         restriction_type = "whitelist"
-        locations        = ["US", "CA", "GB", "DE"]
+        locations        = ["US", "CA", "GB", "DE"] # in order to restrict the traffic
         }
     }
 
@@ -41,18 +43,18 @@ resource "aws_cloudfront_distribution" "roboshop" {
     )
 
     viewer_certificate {
-        acm_certificate_arn = local.acm_certificate_arn
+        acm_certificate_arn = local.acm_certificate # for cdn we need to attach policy
         ssl_support_method = "sni-only"
     }
 }
 
 resource "aws_route53_record" "cloudfront_route53" {
     zone_id = var.route53_zone_id
-    name = "cdn.${var.environment}.${var.route53_domain_name}"
+    name = "cdn.${var.environment}.${var.route53_domain_name}" # domain name of cdn
     type = "A"
     alias {
-     name = aws_cloudfront_distribution.roboshop.domain_name
-     zone_id = aws_cloudfront_distribution.roboshop.hosted_zone_id
+     name = aws_cloudfront_distribution.roboshop.domain_name # storing cdn domain_name in route53
+     zone_id = aws_cloudfront_distribution.roboshop.hosted_zone_id #zone_id is required for route53 in order to add the record
      evaluate_target_health = true
     }
 }
